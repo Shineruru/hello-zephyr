89252c873a075c16d86b78947f4917c43c7dedc1
LULU <shineruru@hotmail.com>
2025-10-21 13:31:13 +0800

    feat: Implement LED turn-on functionality

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 void main(void)
 {
-    printk("Hello World from %s!\n", CONFIG_BOARD);
+	int ret;
+
+	if (!device_is_ready(led.port)) {
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	gpio_pin_set_dt(&led, 1);
 }
