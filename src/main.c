ffa5827c466e63a2bb63aa30e423f3d6d69e867b
LULU <shineruru@hotmail.com>
2025-10-22 14:53:38 +0800

    feat(LED): add user button low to switch toggling 500ms to 100ms

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+/* LED */
 #define LED0_NODE DT_ALIAS(led0)
 
 #if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
@@ -10,11 +11,17 @@
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+/* Button */
+#define SW0_NODE DT_ALIAS(sw0)
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+               
 void main(void)
 {
 	int ret;
+	int sleep_ms = 500;
 
-	if (!device_is_ready(led.port)) {
+	if (!device_is_ready(led.port) || !device_is_ready(button.port)) {
 		return;
 	}
 
@@ -23,11 +30,21 @@ void main(void)
 		return;
 	}
 	
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (ret < 0) {
+        return;
+    }
+
 	while(1) {
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
 			return;
 		}
-		k_msleep(500);
+
+        int val = gpio_pin_get_dt(&button);                                                           
+        if (val >= 0) {                                                                               
+            sleep_ms = (val > 0) ? 100 : 500;                                                         
+        }
+		k_msleep(sleep_ms);
 	}
 }
